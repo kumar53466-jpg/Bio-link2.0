beec48400427c072ffd28394b284fa0273afa3de
𝚨𝛂ѕͱ꧊𝐢͢к <teamaashik@gmail.com>
2025-12-03 22:53:17 +0530

    Update import statement for Biolink module

--- Biolink/modules/dev.py
+++ Biolink/modules/dev.py
@@ -8,7 +8,7 @@ from io import StringIO
 from time import time
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-from MAFU import MAFU as app
+from Biolink import Biolink as app
 from config import OWNER_ID
 
 async def aexec(code, client, message):
