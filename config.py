46ab5447d2ae2305fdd9908efacb9a4cb43034f5
kumar53466-jpg <nikhilkumar53466@gmail.com>
2026-01-15 15:09:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,19 +8,19 @@ API_HASH = os.environ.get("API_HASH", "c17e4eb6d994c9892b8a8b6bfea4042a")
 
 # ------------------------------------------------
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8550727687:AAHuuN0dXOXdIW1-AcvQ21To06dLMtVZsI4")
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "Mafiyamusic_bot")
 
 # -----------------------------------------------
 
-OWNER_ID = int(os.environ.get("OWNER_ID", "8056154987"))
-SPECIAL_ID = int(os.environ.get("SPECIAL_ID", "8056154987"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "8364016757"))
+SPECIAL_ID = int(os.environ.get("SPECIAL_ID", "8364016757"))
 # ------------------------------------------------
 
 # ------------------------------------------------
-LOGGER_ID = int(os.environ.get("LOGGER_ID", ""))
+LOGGER_ID = int(os.environ.get("LOGGER_ID", "1003515759066"))
 
-OTHER_LOGS = int(os.environ.get("OTHER_LOGS", ""))
+OTHER_LOGS = int(os.environ.get("OTHER_LOGS", "1002572229131"))
 
 # ------------------------------------------------
 
